e09826fb17f51098785eaac487b727ac543c6584
Cristian Dragan <cristiandragan34@yahoo.ro>
2025-02-19 19:04:07 +0200

    Implemented tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Georgia', 'serif'],
+        mono: ['"Courier New"', 'monospace'],
+      },
+      colors: {
+        primary: '#634AFF',
+        secondary: 'rgba(99,74,255,0.05)',
+        accent: '#38c172',
+        primaryText: '#11181C',
+        secondaryText: '#6B7280',
+        tertiaryText: '#7C7C7C',
+        background: '#f9fafb',
+        dark: '#1f2937',
+        darkBackground: '#2d3748',
+      },
+    },
+  },
+  plugins: [],
+};
