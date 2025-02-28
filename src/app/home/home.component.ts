97fea00e71c572de96b450e79d9383a9a505adb4
Cristian Dragan <cristiandragan34@yahoo.ro>
2025-02-28 18:34:16 +0200

    Small home screen fix for posts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,41 +2,61 @@ import { Component } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { getDatabase, onValue, ref } from 'firebase/database';
 import { Post } from '../types/post.types';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { PostCardComponent } from '../explore/post-card/post-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NgForOf, PostCardComponent],
+  imports: [NgForOf, PostCardComponent, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   currentUserId: string | null = null;
   posts: { postId: string; postValue: Post }[] = [];
+  following: Record<string, boolean> = {};
+  followers: Record<string, boolean> = {};
   db = getDatabase();
   postRef = ref(this.db, 'posts');
+  userRef: any;
 
   constructor(private auth: Auth) {
     this.auth.onAuthStateChanged((user) => {
       if (user) {
         this.currentUserId = user.uid;
+        this.userRef = ref(this.db, `users/${this.currentUserId}`);
 
-        onValue(this.postRef, (snapshot) => {
+        onValue(this.userRef, (snapshot) => {
           if (snapshot.exists()) {
-            this.posts = Object.entries(snapshot.val())
-              .map(([postId, postData]) => ({
-                postId,
-                postValue: postData as Post,
-              }))
-              .filter((post) => post.postValue.authorId !== this.currentUserId)
-              .sort((a, b) => b.postValue.timestamp - a.postValue.timestamp);
+            const userData = snapshot.val();
+            this.following = userData.following || {};
+            this.followers = userData.followers || {};
 
-            this.fetchProfilePicturesForPosts();
-          } else {
-            this.posts = [];
+            this.fetchPosts();
           }
         });
+      }
+    });
+  }
+
+  fetchPosts() {
+    onValue(this.postRef, (snapshot) => {
+      if (snapshot.exists()) {
+        this.posts = Object.entries(snapshot.val())
+          .map(([postId, postData]) => ({
+            postId,
+            postValue: postData as Post,
+          }))
+          .filter(
+            (post) =>
+              this.following[post.postValue.authorId] ||
+              this.followers[post.postValue.authorId],
+          )
+          .sort((a, b) => b.postValue.timestamp - a.postValue.timestamp);
+
+        this.fetchProfilePicturesForPosts();
+      } else {
+        this.posts = [];
       }
     });
   }
